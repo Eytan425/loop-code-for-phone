bb92858cf3e4171c80c5fc3be5e51e291372dad1
Eytan425 <75117352+Eytan425@users.noreply.github.com>
2022-01-13 19:01:25 +0200

    Update Source.c

--- Source.c
+++ Source.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
+void main() {
 	int i, atmp = 0;
 	printf("Enter the password: \n");
-	scanf_s("%d", &i);
+	scanf("%d", &i);
 	while (i != 2007 && atmp <4)
 	{
 		atmp++;
 		printf("You did..... NOT GET IN!!! HOW CAN YOU FORGET THE CODE YOU MORON \n");
-		scanf_s("%d", &i);
+		scanf("%d", &i);
 	}
 	if (atmp < 4)
 	{
-		printf("You are in!!");
+		printf("You are in!!\nGood day");
 	}
 	else
 	{
 		printf("Too many tries, good day\n");
 	}
-	return 0;
+	
 
 }
